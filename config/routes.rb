dfac25607696d9a2b7960abe269e94040533c350
Homies886 <Niko876@hotmail.com>
2020-08-25 12:10:59 -0400

    Namespacing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :captures
-  resources :pokemons
-  resources :games
-  resources :users
+  namespace :api do
+    namespace :v1 do
+      resources :captures
+      resources :pokemons
+      resources :games
+      resources :users
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
