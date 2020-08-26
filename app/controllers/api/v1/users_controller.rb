4ab3001bd03600b097544297276047c1005e2b77
Homies886 <Niko876@hotmail.com>
2020-08-26 12:51:43 -0400

    set up a fake user and user controller actions

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,41 @@
 class Api::V1::UsersController < ApplicationController
+    before_action :set_user, only: [:show, :update, :destroy]
+
+    def index
+        users = User.all
+        render json: UserSerializer.new(users)
+    end
+
+    def show
+        user_json = UserSerializer.new(@user).serialized_json
+        render json: user_json
+    end
+
+    def create
+        user = User.new(user_params)
+        if user.save
+            session[:user_id] = user.id
+            render json: UserSerializer.new(user), status: :created
+        else
+            render json: { errors: user.errors.full_messages }, status: :unprocessible_entity
+        end
+    end
+
+    def update
+
+    end
+
+    def destroy
+        @user.destroy
+    end
+
+    private
+
+    def set_user
+        @user = User.find(params[:id])
+    end
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
     has_secure_password
     has_many :captures
     has_many :pokemons
+
+    validates :username, presence: true
+    validates :username, uniqueness: true
 end
